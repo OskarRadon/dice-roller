b2217589e72aeaf49d84c9987ccc9d0938a184e2
Taylor and Jay <student@epicodus.com>
2016-02-01 09:32:49 -0800

    pass test two

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,15 @@ function Dice(sides, rolls, modifier) {
 Dice.prototype.play = function () {
   var min = 1;
   var max = this.sides;
-  var counter  = 0;
+  var counter  = 1;
   var totalRolls =[];
+  var numberHolder = 0;
   while (counter <= this.rolls) {
     counter++;
     totalRolls.push(Math.floor(Math.random() * (max - min +1)) + min);
   }
-  return false;
+  totalRolls.forEach(function(index)  {
+    numberHolder = index;
+  });
+  return numberHolder;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -7,6 +7,6 @@ describe("Dice", function(){
   });
   it("will generate random number based on number of sides of dice, number of rolls, and modifier selected", function(){
     var testDice = new Dice(6, 3, 2);
-    expect(testDice.play()).to.equal(false);
+    expect(isNaN(testDice.play())).to.equal(false);
   });
 });

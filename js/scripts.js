455c462f580ec55727e60cb3e3ebbff23ba23250
Taylor and Jay <student@epicodus.com>
2016-02-01 09:19:19 -0800

    Pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,16 @@ function Dice(sides, rolls, modifier) {
   this.sides = sides;
   this.rolls = rolls;
   this.modifier = modifier;
+};
 
+Dice.prototype.play = function () {
+  var min = 1;
+  var max = this.sides;
+  var counter  = 0;
+  var totalRolls =[];
+  while (counter <= this.rolls) {
+    counter++;
+    totalRolls.push(Math.floor(Math.random() * (max - min +1)) + min);
+  }
+  return false;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,12 @@
 describe("Dice", function(){
   it("will determine the roll of the dice depending on input of how many sides, number of rolls and modifier selected", function(){
-    var testDice = new Dice(6, 3, 2)
+    var testDice = new Dice(6, 3, 2);
     expect(testDice.sides).to.equal(6);
     expect(testDice.rolls).to.equal(3);
     expect(testDice.modifier).to.equal(2);
-  })
-})
+  });
+  it("will generate random number based on number of sides of dice, number of rolls, and modifier selected", function(){
+    var testDice = new Dice(6, 3, 2);
+    expect(testDice.play()).to.equal(false);
+  });
+});
